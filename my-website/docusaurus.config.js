1153bc2ba1cab0098583b4bf1b55550641711e22
Elizaveta Maryina <liza-nn02@mail.ru>
2026-05-07 00:38:12 +0300

    fix config

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
 
 
   plugins: [
-/*    [
+    [
       'redocusaurus',
       {
         specs: [
@@ -29,7 +29,7 @@ const config = {
           primaryColor: '#1890ff',
         },
       },
-    ],*/
+    ],
   ],
 
   presets: [
